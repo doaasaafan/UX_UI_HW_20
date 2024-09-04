48706281a2c2921cc35a033d4529e163e5e73cfc
doaasaafan <doaasaafan16@gmail.com>
2024-09-04 01:09:13 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//index.js
+
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+    const hamIcon = this.querySelector('.hamburger-icon'); 
+    const crossIcon = this.querySelector('.cross-icon'); 
+    if (hamIcon.style.display === "none") { 
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    } 
+    else { 
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    } 
+});
+
+var myVideo = document.getElementById("hopeforchildren"); 
+
+function playPause() { 
+  if (myVideo.paused) 
+    myVideo.play(); 
+  else 
+    myVideo.pause(); 
+} 
